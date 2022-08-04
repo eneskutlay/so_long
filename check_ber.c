e3419d5cd093f53eac0d4d34dba969026c9d3bb0
Enes Kutlay <contact@eneskutlay.dev>
2022-08-05 00:46:34 +0300

    invalid extension error fixed

--- check_ber.c
+++ check_ber.c
@@ -6,7 +6,7 @@
 /*   By: ekutlay <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 02:22:55 by ekutlay           #+#    #+#             */
-/*   Updated: 2022/07/30 02:26:06 by ekutlay          ###   ########.fr       */
+/*   Updated: 2022/08/05 00:27:22 by ekutlay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ void	check_ber(char *argv)
 {
 	int	i;
 	int	e;
+	int	l;
 
 	i = 0;
 	e = 0;
+	l = ft_strlen_lib(argv);
 	while (argv[i] != '.')
 		i++;
 	i++;
@@ -29,6 +31,8 @@ void	check_ber(char *argv)
 		e++;
 	i++;
 	if (argv[i] != 'r')
+		e++;
+	if (argv[i] != l)
 		e++;
 	if (e != 0)
 	{
